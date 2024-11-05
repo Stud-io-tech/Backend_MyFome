a17f233c13f6b704d55ba8b914734b20b0df1b16
Welen1911 <welenalmeida2210@gmail.com>
2024-11-04 23:03:16 -0300

    Sistema de login

---
+++ projetos/backend/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $user = User::where('email', $credentials['email'])->first();
+
+        if (!$user) {
+
+            return response(null, 404);
+        }
+
+        Auth::login($user);
+
+        $token = Auth::user()->createToken('login_token');
+
+        return response(['token' => $token->plainTextToken], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy()
+    {
+        Auth::user()->currentAccessToken()->delete();
+
+        return response(null, 200);
+    }
+}
--- projetos/backend/routes/api.php
+++ projetos/backend/routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'store']);
+Route::delete('/logout', [AuthController::class, 'destroy'])->middleware(['auth:sanctum']);
+
+Route::post('/register', [UserController::class, 'store']);
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
