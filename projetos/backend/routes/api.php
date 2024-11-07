32b3ea2ef7d640f3dadc11f791577b1a68d4e885
Welen1911 <welenalmeida2210@gmail.com>
2024-11-06 23:23:41 -0300

    Crud de loja feito

--- projetos/backend/routes/api.php
+++ projetos/backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\StoreController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,10 @@ Route::delete('/logout', [AuthController::class, 'destroy'])->middleware(['auth:
 
 Route::post('/register', [UserController::class, 'store']);
 
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/store', StoreController::class);
+});
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
